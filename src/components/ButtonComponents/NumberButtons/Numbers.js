44db551e9f129751dc18f7aacff43f9bc2d13c01
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-07-27 09:07:28 -0700

    Added button components.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //import any components needed
-import { numbers } from '../src/data.js';
+import NumberButton from './NumberButton.js';
 
 //Import your array data to from the provided data file
+import { numbers } from '../src/data.js';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+
+  const [numberState, setNumberState] = useState(numbers);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbers.map(number => {
+        return <NumberButton numbers={number} />;
+      })}
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //import any components needed
-import { operators } from '../src/data.js';
+import OperatorButton from './OperatorButton.js';
 
 //Import your array data to from the provided data file
+import { operators } from '../src/data.js';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+
+  const [operatorState, setOperatorState] = useState(operators);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operators.map(operator => {
+        return <OperatorButton operators={operator} />;
+      })}
     </div>
   );
 };
+
+export default Operators;
